14710057d7e146b6e3c6066ce55cb51390e78e5b
Jose Antonio Marquez <jmarquez@twitter.com>
2016-01-03 12:32:02 -0800

    Adding user switching and updating assets

--- app/mobile/Jacob/platforms/ios/www/js/index.js
+++ app/mobile/Jacob/platforms/ios/www/js/index.js
@@ -2,6 +2,18 @@
  *
  */
 var app = {
+  
+  /**
+   *
+   */
+  currentKid: 0,
+  
+  /**
+   *
+   */
+  getCurrentKid: function getCurrentKid() {
+    return app.currentKid;
+  },
 
   /**
    *
@@ -21,13 +33,49 @@ var app = {
     var badBtn = document.querySelector('.action .slds-icon-action-close');
     var goodBtn = document.querySelector('.action .slds-icon-action-approval');
     
+    var kid1Btn = document.querySelector('.kids .kid1');
+    var kid2Btn = document.querySelector('.kids .kid2');
+    var kid3Btn = document.querySelector('.kids .kid3');
+    kid1Btn.addEventListener('touchend', function () {
+      app.selectKid(1);
+    });
+    kid2Btn.addEventListener('touchend', function () {
+      app.selectKid(2);
+    });
+    kid3Btn.addEventListener('touchend', function () {
+      app.selectKid(3);
+    });
+    
+    
     badBtn.addEventListener('touchend', function () {
-      app.makeApiRequest('POST', 'http://ouradventureproject.com/deepak/score.php', 'familyId=1&childId=' + 3 + '&score=1');
+      app.makeApiRequest('POST', 'http://ouradventureproject.com/deepak/score.php', 'familyId=1&childId=' + app.getCurrentKid() + '&score=1');
     });
     
     goodBtn.addEventListener('touchend', function () {
-      app.makeApiRequest('POST', 'http://ouradventureproject.com/deepak/score.php', 'familyId=1&childId=' + 3 + '&score=-1');
+      app.makeApiRequest('POST', 'http://ouradventureproject.com/deepak/score.php', 'familyId=1&childId=' + app.getCurrentKid() + '&score=-1');
     });
+    
+    app.selectKid(3);
+  },
+  
+  /**
+   *
+   */
+  selectKid: function selectKid(kid) {
+    app.resetKidSelection();
+    var kidEl = document.querySelector('.kids .kid' + kid);
+    kidEl.setAttribute('data-status', 'selected');
+    app.currentKid = kid;
+  },
+  
+  /**
+   *
+   */
+  resetKidSelection: function resetKidSelection () {
+    var kids = document.querySelectorAll('.kids .kid');
+    for (var i = 0, currKid; (currKid = kids[i]); i++) {
+      currKid.removeAttribute('data-status');
+    }
   },
   
   /**
